f4c553765fdcab535a5e98195121eab2cab51ded
shreyakulal55 <shreyakulal55@gmail.com>
2023-11-01 19:30:19 +0530

    deply

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,48 +1,14 @@
 const express = require('express');
-// const app = express();
+const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes');
-
-// // const express = require('express');
-// const path = require('path');
-// // const app = express();
-
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-// app.listen(9000);
-
-
-
-const express = require('express');
-const path = require('path');
-const app = express();
-
-// Serve static files from the "build" directory
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Define a catch-all route to serve "index.html"
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-// Start the Express server
-app.listen(9000, () => {
-  console.log('Server is running on port 9000');
-});
-
-
-
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/', routes);
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
