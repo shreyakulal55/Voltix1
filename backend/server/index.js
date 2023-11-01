1d25f29632d718f3e9d9ffe731de49e5f34af8d2
shreyakulal55 <shreyakulal55@gmail.com>
2023-11-01 19:10:25 +0530

    deply

--- backend/server/index.js
+++ backend/server/index.js
@@ -31,9 +31,9 @@ app.get('*', (req, res) => {
 });
 
 // Start the Express server
-// app.listen(9000, () => {
-//   console.log('Server is running on port 9000');
-// });
+app.listen(9000, () => {
+  console.log('Server is running on port 9000');
+});
 
 
 
@@ -42,7 +42,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/', routes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
