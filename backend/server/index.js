4b1559f672a7f17759dcda954aa8e266f6f62b9b
shreyakulal55 <shreyakulal55@gmail.com>
2023-11-01 18:50:54 +0530

    deply

--- backend/server/index.js
+++ backend/server/index.js
@@ -4,6 +4,24 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes');
 
+const express = require('express');
+const path = require('path');
+// const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
+
+
+
+
+
+
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/', routes);
