bf3d697d6e9077e77bff10adc86d725328225b85
shreyakulal55 <shreyakulal55@gmail.com>
2023-11-01 19:02:27 +0530

    deply

--- backend/server/index.js
+++ backend/server/index.js
@@ -10,11 +10,11 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3000);
+app.listen(9000);
 
 
 
