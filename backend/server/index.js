4fe16cdbea741f2f6d6ae0f57246f2b8f0a017cd
shreyakulal55 <shreyakulal55@gmail.com>
2023-11-03 13:09:32 +0530

    test

--- backend/server/index.js
+++ backend/server/index.js
@@ -4,36 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes');
 
-// // const express = require('express');
-// const path = require('path');
-// // const app = express();
-
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-// app.listen(9000);
-
-
-
-// // const express = require('express');
-// const path = require('path');
-// // const app = express();
-
-// // Serve static files from the "build" directory
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// // Define a catch-all route to serve "index.html"
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-// // Start the Express server
-// app.listen(9000, () => {
-//   console.log('Server is running on port 9000');
-// });
 
 
 
--- frontend/src/.env/secret.js
+++ frontend/src/.env/secret.js
@@ -1,2 +1,2 @@
-const URL= "http://3.80.223.186";
+const URL= "http://localhost:5000";
 export default URL;
